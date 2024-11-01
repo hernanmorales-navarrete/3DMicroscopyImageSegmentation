ade6f0a46706ad64dba2d3b6483495feb8cd850e
WestSopho <153777405+WestSopho@users.noreply.github.com>
2024-11-01 20:09:07 +0000

    define image, mask and predictions and title for the plotting function

--- utils/data_viz.py
+++ utils/data_viz.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 
-def visualize_patch_3D_in_2D(image, mask, z):
+def visualize_patch_3D_in_2D(image, mask, prediction, title, z):
     img_2D = image[:, :, z]
     mask_2D = mask[:, :, z]
+    pred_2D = prediction[:, :, z]
 
-    fig, axes = plt.subplots(1, 2, figsize=(6, 6))
+    fig, axes = plt.subplots(1, 3, figsize=(6, 6))
 
     axes[0].imshow(img_2D, cmap='gray')
     axes[0].axis('off')
@@ -13,6 +14,11 @@ def visualize_patch_3D_in_2D(image, mask, z):
     axes[1].imshow(mask_2D, cmap='gray')
     axes[1].axis('off')
     axes[1].set_title('Mask')
-
-    plt.tight_layout()
+    
+    axes[2].imshow(pred_2D, cmap='gray')
+    axes[2].axis('off')
+    axes[2].set_title('Prediction')
+    
+    fig.suptitle(title)
+    fig.tight_layout()
     plt.show()
