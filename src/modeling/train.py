e5760ba3c762cec1972b7fa7dca69f3d6f354f02
WestSopho <153777405+WestSopho@users.noreply.github.com>
2025-04-22 03:51:28 +0000

    feat: configure GPU memory growth in training script to optimize resource usage

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -9,6 +9,19 @@ from enum import Enum
 import os
 from sklearn.model_selection import train_test_split
 
+# Configure GPU memory growth - avoid taking all memory
+gpus = tf.config.list_physical_devices("GPU")
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.list_logical_devices("GPU")
+        logger.info(f"Using {len(logical_gpus)} Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        logger.error(e)
+
 from src.config import (
     MODELS_DIR,
     LOGS_DIR,
